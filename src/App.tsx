811bf4db92065ea7ccc4799a6c5be49567b75a10
SealWifBuds <info@sealwifbuds.com>
2025-02-14 14:37:44 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,8 @@
-import { createAppKit } from '@reown/appkit/react'
-import { WagmiProvider } from 'wagmi'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { projectId, metadata, networks, wagmiAdapter } from './config'
+import { Connected } from './components/Connected'
+import { Text } from './components/Text'
 
 import "./App.css"
 
-const queryClient = new QueryClient()
-
-const generalConfig = {
-  projectId,
-  networks,
-  metadata,
-  themeMode: 'dark' as const,
-  themeVariables: {
-    '--w3m-accent': '#0d76fc',
-  }
-}
-
-// Create modal
-createAppKit({
-  adapters: [wagmiAdapter],
-  ...generalConfig,
-  features: {
-    analytics: true // Optional - defaults to your Cloud configuration
-  }
-})
 
 export function App() {
   return (
@@ -35,11 +13,8 @@ export function App() {
         </h1>
         <p><img src="/logo.png" alt="Logo" style={{ width: '200px', height: '200px' }} /></p>
         <br/>
-        <WagmiProvider config={wagmiAdapter.wagmiConfig}>
-          <QueryClientProvider client={queryClient}>
-            <appkit-button/>
-          </QueryClientProvider>
-        </WagmiProvider>
+        <Text/>
+            <Connected/>
       </div>
     </div>
   )
